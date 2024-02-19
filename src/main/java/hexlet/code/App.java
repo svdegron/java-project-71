449b0fa5762cbec910a6cd093f865b399fffc994
Alexander Kamenetsky <svdegron@gmail.com>
2024-02-20 04:38:18 +0500

    Add picocli with the specified parameters -hV

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,28 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(
+    name = "gendiff",
+    version = "gendiff 0.1",
+    description = "Compares two configuration files and show a difference."
+)
+public class App implements Runnable {
+
+    @Option(names = { "-V", "--version"}, versionHelp = true)
+    private boolean versionInfoRequested;
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this message and exit.")
+    private boolean usageHelpRequested = false;
+
     public static void main(String[] args) {
+        new CommandLine(new App()).execute(args);
+    }
+
+    @Override
+    public void run() {
         System.out.println("Hello World!");
     }
 }
