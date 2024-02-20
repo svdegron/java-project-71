5c2f8396932130e39667f5e09c1cd665c1c16233
Alexander Kamenetsky <svdegron@gmail.com>
2024-02-20 06:33:57 +0500

    Change App update help text

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,6 +2,9 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import java.nio.file.Path;
 
 @Command(
     name = "gendiff",
@@ -11,12 +14,21 @@ import picocli.CommandLine.Command;
 )
 public class App implements Runnable {
 
-    public static void main(String[] args) {
-        new CommandLine(new App()).execute(args);
-    }
+    @Option(names = { "-f", "--format" }, description = "output format [default: stylish]")
+    private String format;
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private Path filepath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private Path filepath2;
 
     @Override
     public void run() {
         System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        new CommandLine(new App()).execute(args);
     }
 }
