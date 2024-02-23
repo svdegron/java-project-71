8fc09e8e70a54060d2e0c366fa73779f65c1a52e
Alexander Kamenetsky <svdegron@gmail.com>
2024-02-23 07:45:48 +0500

    Change file path type method Differ.generate takes strings

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,9 +4,6 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-
-import java.nio.file.Path;
-//??? import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 
 @Command(
@@ -21,10 +18,10 @@ public class App implements Callable<Integer> {
     private String format;
 
     @Parameters(paramLabel = "filepath1", description = "path to first file")
-    private Path filepath1;
+    private String filepath1;
 
     @Parameters(paramLabel = "filepath2", description = "path to second file")
-    private Path filepath2;
+    private String filepath2;
 
     @Override
     public Integer call() throws Exception {
--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -8,17 +8,25 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 //??? import java.nio.file.Paths;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Differ {
 
-    public static String generate(Path filepath1, Path filepath2) throws IOException {
+    public static String generate(String filepath1, String filepath2) throws IOException {
+        System.out.println("String");
         System.out.println("filepath1 | " + filepath1);
         System.out.println("filepath2 | " + filepath2);
-        System.out.println("exists | " + Files.exists(filepath1));
-        var contentFile1 = Files.readString(filepath1);
-        var contentFile2 = Files.readString(filepath2);
+        System.out.println("to path");
+        Path path1 = Paths.get(filepath1);
+        Path path2 = Paths.get(filepath2);
+        System.out.println("path1 | " + path1);
+        System.out.println("path2 | " + path2);
+        System.out.println("exists 1 | " + Files.exists(path1));
+        System.out.println("exists 1 | " + Files.exists(path2));
+        var contentFile1 = Files.readString(path1);
+        var contentFile2 = Files.readString(path2);
 
         Map<String, String> json1 = new HashMap<>();
         Map<String, String> json2 = new HashMap<>();
