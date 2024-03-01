f9afda919f51bd744a100806236ce6ea326dd097
Alexander Kamenetsky <svdegron@gmail.com>
2024-03-02 04:18:33 +0500

    Check first test - fail

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -1,4 +1,13 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DifferTest {
+
+    @Test
+    public void generateSuccess() {
+        assertEquals(1, 2);
+    }
 }
