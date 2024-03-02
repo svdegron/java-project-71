b53298a5c2973c2cc2d90a601a99b711dbf776d2
Alexander Kamenetsky <svdegron@gmail.com>
2024-03-02 05:33:54 +0500

    Update tests for directory and files - success

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -9,7 +9,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -19,18 +18,13 @@ public class DifferTest {
     private final Path resourceDirectory = Paths.get("src","test", "resources");
     private static String correctResourceDirectory;
 
-    private static final int testFilesCount = 4;
-    private static List<String> fileNames;
+    // Количество файлов для тестирования
+    private static final int testFilesCount = 3;
 
     @BeforeAll
     public static void beforeAll() {
         String[] pathPieces = { "src", "test", "resources" };
         correctResourceDirectory = String.join(File.separator, pathPieces);
-
-        fileNames = new LinkedList<>();
-        for (var iStep = 1; iStep <= testFilesCount; iStep++) {
-            fileNames.add("file" + iStep + ".json");
-        }
     }
 
     @Test
@@ -47,36 +41,18 @@ public class DifferTest {
 
     @Test
     public void checkFiles() {
-//        String resourceName = "file1.json";
-//
-//        ClassLoader classLoader = getClass().getClassLoader();
-//        File file = new File(Objects.requireNonNull(classLoader.getResource(resourceName)).getFile());
-//        String absoluteFilePath = file.getAbsolutePath();
-//
-//        System.out.println("absoluteFilePath | " + absoluteFilePath);
-//        System.out.println("fileNames | " + fileNames);
-//
-//        String absolutePath = resourceDirectory.toFile().getAbsolutePath();
-
-//        var isExist = Files.exists(Paths.get(absolutePath, resourceName));
-//        System.out.println("isExist | " + isExist);
-
-        var existPath = new LinkedList<Boolean>();
-        var expectedList = new  LinkedList<Boolean>();
-        String absolutePath = resourceDirectory.toFile().getAbsolutePath();
+        List<Boolean> existPath = new LinkedList<>();
+        List<Boolean> expectedList = new LinkedList<>();
+        String absoluteDirectoryPath = resourceDirectory.toFile().getAbsolutePath();
 
         for (var iStep = 1; iStep <= testFilesCount; iStep++) {
             var fileName = "file" + iStep + ".json";
-            var filePath = Paths.get(absolutePath, fileName);
+            var filePath = Paths.get(absoluteDirectoryPath, fileName);
             var isExist = Files.exists(filePath);
 
             existPath.add(isExist);
             expectedList.add(true);
         }
-
-//        assertTrue(absoluteFilePath.endsWith(File.separator + resourceName));
-        System.out.println("expectedList | " + expectedList);
-        System.out.println("existPath | " + existPath);
 
         assertEquals(expectedList, existPath);
     }
