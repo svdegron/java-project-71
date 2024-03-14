f24e17402ee89fc2be64d38506389394505bca6d
Alexander Kamenetsky <svdegron@gmail.com>
2024-03-14 13:26:06 +0500

    CHANGE classes - move parse content from Differ.java to Parser.java

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,13 +1,7 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
 import java.util.LinkedList;
 import java.util.Set;
 import java.util.TreeSet;
@@ -15,49 +9,34 @@ import java.util.TreeSet;
 public class Differ {
 
     public static String generate(String filepath1, String filepath2) throws IOException {
-        Path path1 = Paths.get(filepath1);
-        Path path2 = Paths.get(filepath2);
-
-        if (!Files.exists(path1)) {
-            throw new IOException("Check that the file \"" + path1 + "\" exists and can be accessed");
-        }
-
-        if (!Files.exists(path2)) {
-            throw new IOException("Check that the file \"" + path2 + "\" exists and can be accessed");
-        }
-
-        var content1 = Files.readString(path1);
-        var content2 = Files.readString(path2);
-
-        var objectMapper = new ObjectMapper();
-        var json1 = objectMapper.readValue(content1, new TypeReference<Map<String, Object>>() { });
-        var json2 = objectMapper.readValue(content2, new TypeReference<Map<String, Object>>() { });
+        var fileMap1 = Parser.getMap(Paths.get(filepath1));
+        var fileMap2 = Parser.getMap(Paths.get(filepath2));
 
         var results = new LinkedList<String>();
 
-        Set<String> allKeys = new TreeSet<>(json1.keySet());
-        allKeys.addAll(json2.keySet());
+        Set<String> allKeys = new TreeSet<>(fileMap1.keySet());
+        allKeys.addAll(fileMap2.keySet());
 
         for (String key : allKeys) {
-            if (json1.containsKey(key) && json1.containsKey(key)) {
-                if (json1.get(key).equals(json2.get(key))) {
-                    results.add("    " + key + ": " + json1.get(key));
+            if (fileMap1.containsKey(key) && fileMap1.containsKey(key)) {
+                if (fileMap1.get(key).equals(fileMap2.get(key))) {
+                    results.add("    " + key + ": " + fileMap1.get(key));
                 } else {
-                    if (json1.get(key) != null) {
-                        results.add("  - " + key + ": " + json1.get(key));
+                    if (fileMap1.get(key) != null) {
+                        results.add("  - " + key + ": " + fileMap1.get(key));
                     }
 
-                    if (json2.get(key) != null) {
-                        results.add("  + " + key + ": " + json2.get(key));
+                    if (fileMap2.get(key) != null) {
+                        results.add("  + " + key + ": " + fileMap2.get(key));
                     }
                 }
             } else {
-                if (json1.get(key) != null) {
-                    results.add("  - " + key + ": " + json1.get(key));
+                if (fileMap1.get(key) != null) {
+                    results.add("  - " + key + ": " + fileMap1.get(key));
                 }
 
-                if (json2.get(key) != null) {
-                    results.add("  + " + key + ": " + json2.get(key));
+                if (fileMap2.get(key) != null) {
+                    results.add("  + " + key + ": " + fileMap2.get(key));
                 }
             }
         }
--- src/main/java/hexlet/code/Parser.java
+++ src/main/java/hexlet/code/Parser.java
@@ -1,4 +1,22 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
 public class Parser {
+    public static Map<String, Object> getMap(Path path) throws IOException {
+        if (!Files.exists(path)) {
+            throw new IOException("Check that the file \"" + path + "\" exists and can be accessed");
+        }
+
+        var content = Files.readString(path);
+
+        var objectMapper = new YAMLMapper();
+
+        return objectMapper.readValue(content, Map.class);
+    }
 }
