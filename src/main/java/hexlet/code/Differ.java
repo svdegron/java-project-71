2fc280f959a85ceff3b3f2b93ee0294b48491d66
Alexander Kamenetsky <svdegron@gmail.com>
2024-03-17 17:56:49 +0500

    изменил класс - доработал логику, добавил методы формирования списка

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -3,62 +3,60 @@ package hexlet.code;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
-
-import static hexlet.code.Formatter.addItem;
 
 public class Differ {
+
+    private static List<String> addResult(String action, Object value) {
+        return addResult(action, value, "");
+    }
+
+    private static List<String> addResult(String action, Object value1, Object value2) {
+        var items = new ArrayList<String>();
+        items.add(action);
+        items.add(value1.toString());
+
+        if (!"".equals(value2)) {
+            items.add(value2.toString());
+        }
+
+        return items;
+    }
 
     public static String generate(String filepath1, String filepath2, String format) throws IOException {
         var fileMap1 = Parser.getMap(Paths.get(filepath1));
         var fileMap2 = Parser.getMap(Paths.get(filepath2));
 
-        var results = new TreeMap<String, String>();
+        var results = new LinkedHashMap<String, List<String>>();
 
         for (var key : fileMap1.keySet()) {
             var entry1 = fileMap1.get(key);
-            String result;
 
             if (fileMap2.containsKey(key)) {
                 var entry2 = fileMap2.get(key);
 
                 if (entry1.equals(entry2)) {
-                    result = addItem(format, Formatter.EQUAL, key, entry1);
-                    results.put(key, result);
+                    results.put(key, addResult(Formatter.EQUAL, entry1));
                 } else {
-                    result = addItem(format, Formatter.EDIT, key, entry1, entry2);
-                    results.put(key, result);
+                    results.put(key, addResult(Formatter.EDIT, entry1, entry2));
                 }
 
                 fileMap2.remove(key);
             } else {
-                result = addItem(format, Formatter.DELETE, key, entry1);
-                results.put(key, result);
+                results.put(key, addResult(Formatter.DELETE, entry1));
             }
         }
 
         for (var key : fileMap2.keySet()) {
-            var result = addItem(format, Formatter.ADD, key, fileMap2.get(key));
-            results.put(key, result);
+            results.put(key, addResult(Formatter.ADD, fileMap2.get(key)));
         }
 
         if (results.isEmpty()) {
             return "Files are empty";
         }
 
-        var result = results.entrySet().stream()
-            .map(Map.Entry::getValue)
-            .filter(line -> line.length() > 0)
-            .collect(Collectors.joining(System.lineSeparator()));
-
-        // В зависимости от операционной системы
-        // переносы строк можут быть разных видов:
-        // LF - For a Unix/Linux/New Mac-based OS
-        // CRLF - on a Windows-based OS
-        return String.join(System.lineSeparator(), "{", result, "}");
+        return Formatter.getResult(results, format);
     }
 
 }
