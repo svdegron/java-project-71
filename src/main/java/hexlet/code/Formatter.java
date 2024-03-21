f148e1fa554ef94f6d169091a0bd329c7ad6c719
Alexander Kamenetsky <svdegron@gmail.com>
2024-03-21 07:38:36 +0500

    Заменил if-else на рабочий switch-case - удалил лишние строки с состояниями пар, т.к. добавлено перечисление; доделать/проверить обработку ошибки JsonProcessingException

--- src/main/java/hexlet/code/Formatter.java
+++ src/main/java/hexlet/code/Formatter.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -8,31 +10,26 @@ import static hexlet.code.formatters.Plain.toPlain;
 import static hexlet.code.formatters.Stylish.toStylish;
 
 public class Formatter {
-
-    public static final String EXIST = "exist";
-    public static final String DELETE = "delete";
-    public static final String ADD = "add";
-    public static final String EDIT = "edit";
-
     public static final int ACTION = 0;
     public static final int FIRST_VALUE = 1;
     public static final int SECOND_VALUE = 2;
 
     public static String getResult(LinkedHashMap<String, List<Object>> map, String format) {
-
-        if ("plain".equals(format)) {
-            return toPlain(map);
-        } else if ("json".equals(format)) {
-            String result = null;
-            try {
-                result = toJson(map);
-            } catch (Exception e) {
-                result = e.getMessage();
+        String result = switch (format) {
+            case "plain" -> toPlain(map);
+            case "json" -> {
+                // продумать обработку исключения
+                try {
+                    yield toJson(map);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
             }
-            return result;
-        } else {
-            return toStylish(map);
-        }
+            case "stylish" -> toStylish(map);
+            default -> "";
+        };
+
+        return result;
     }
 
 }
