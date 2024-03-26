a91a40c7a85f789bbe08ab4abb195c04764d5528
Alexander Kamenetsky <svdegron@gmail.com>
2024-03-27 02:13:49 +0500

    проверка

--- src/main/java/hexlet/code/Parser.java
+++ src/main/java/hexlet/code/Parser.java
@@ -1,16 +1,17 @@
 package hexlet.code;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
     public static Map<String, Object> getMap(String content) throws JsonProcessingException {
-        var objectMapper = new YAMLMapper();
-        return objectMapper.readValue(content, Map.class);
-//        Map<String, Object> map = new HashMap<>();
-//        return map;
+        ObjectMapper mapper = new YAMLMapper();
+        @SuppressWarnings("unchecked")
+        Map<String, Object> result = mapper.readValue(content, Map.class);
+
+        return result;
     }
 }
