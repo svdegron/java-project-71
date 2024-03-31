b6937c233b9eab0e4dc012607982da47302aba6a
Alexander Kamenetsky <svdegron@gmail.com>
2024-03-31 08:24:58 +0500

    action + settings

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -50,3 +50,37 @@ public class Stylish {
         return temp;
     }
 }
+
+/*
+попробовать отсюда
+https://github.com/hexlet-boilerplates/java-package/blob/main/.github/workflows/main.yml
+name: Java CI
+
+on:
+  - push
+  - pull_request
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v3
+      - uses: actions/setup-java@v3
+        with:
+          java-version: '20'
+          distribution: 'temurin'
+      - uses: gradle/gradle-build-action@v2
+        with:
+          gradle-version: 8.3
+      - run: ./gradlew checkstyleMain
+      - run: ./gradlew test
+      # - name: Publish code coverage
+      #   uses: paambaati/codeclimate-action@v5.0.0
+      #   env:
+      #     CC_TEST_REPORTER_ID: ${{secrets.CC_TEST_REPORTER_ID}}
+      #     JACOCO_SOURCE_PATH: src/main/java
+      #   with:
+      #     coverageCommand: make report
+      #     coverageLocations: ${{github.workspace}}/build/reports/jacoco/test/jacocoTestReport.xml:jacoco
+*/
